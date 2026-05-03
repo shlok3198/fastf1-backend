1791001732c8a39ce1ee045e51f02dbec7a59545
shlok3198 <54074657+shlok3198@users.noreply.github.com>
2026-05-03 12:30:56 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import fastf1
+from fastf1 import get_session
+
+app = FastAPI()
+
+fastf1.Cache.enable_cache('./cache')
+
+@app.get("/health")
+def health():
+    return {"ok": True}
+
+@app.get("/api/live")
+def live_data():
+    try:
+        session = get_session(2026, 'Miami', 'R')
+        session.load()
+
+        driver = session.laps.pick_driver('16').pick_fastest()
+
+        if driver is None:
+            return {"status": "no-data"}
+
+        telemetry = driver.get_car_data().add_distance()
+        latest = telemetry.iloc[-1]
+
+        return {
+            "status": "live",
+            "speed": float(latest['Speed']),
+            "throttle": float(latest['Throttle']),
+            "brake": float(latest['Brake']),
+            "gear": int(latest['nGear'])
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
